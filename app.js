53525b189d23443b950a0e362119d8dadb1b5eed
Jeff Horner <jeffnhorner@outlook.com>
2018-03-19 12:50:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+angular.module("test", ["ui-routher]")
+
+//state provider is essetnailly a state/pages for each page
+.config(function($stateProvider, $urlRouterProvider){
+
+//Now list different states/pages you'll page
+$urlRouterProvider.otherwise("/");
+
+$stateProvider
+  .state("home", {
+    //url you type into browers
+    url: '/',
+    //where the browser pulls your page (file) from
+    templateUrl: './views/home.html',
+    // controller: 'mainCtrl'
+  })
+  .state("about"), {
+    url:"/about",
+    templateUrl: "views/about.html",
+    // controller: "mainCtrl"
+  }
+  .state("contact"), {
+    url:"/contact",
+    templateUrl: "views/contact.html",
+    // controller: "mainCtrl"
+  }
+
+
+
+
+})
